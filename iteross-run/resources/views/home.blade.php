4eca6b741ae7c5ffdc6f33ac5ea05c36b724a5a2
Vindma <v9957820668@gmail.com>
2026-07-28 13:13:56 +0300

    правлю баг в шапке

--- iteross-run/resources/views/home.blade.php
+++ iteross-run/resources/views/home.blade.php
@@ -1708,47 +1708,6 @@
 </div>
 
 <script>
-    (function () {
-        var menu = document.querySelector('[data-account-menu]');
-        var trigger = document.querySelector('[data-account-menu-trigger]');
-
-        if (!menu || !trigger) {
-            return;
-        }
-
-        function openMenu() {
-            menu.classList.add('is-open');
-            trigger.setAttribute('aria-expanded', 'true');
-        }
-
-        function closeMenu() {
-            menu.classList.remove('is-open');
-            trigger.setAttribute('aria-expanded', 'false');
-        }
-
-        trigger.addEventListener('click', function (event) {
-            event.stopPropagation();
-
-            if (menu.classList.contains('is-open')) {
-                closeMenu();
-                return;
-            }
-
-            openMenu();
-        });
-
-        document.addEventListener('click', function (event) {
-            if (!menu.contains(event.target)) {
-                closeMenu();
-            }
-        });
-
-        document.addEventListener('keydown', function (event) {
-            if (event.key === 'Escape') {
-                closeMenu();
-            }
-        });
-    })();
 
     (function () {
         document.querySelectorAll('.file-box input[type="file"]').forEach(function (input) {
